0dd008cb60818ee8111eeb2fde5de389cd9fce71
Walter Gordy <walter.gordy@gmail.com>
2024-02-16 09:24:04 -0700

    Updated to Unity 2022. Adds preview gif

--- Assets/ColorManager.cs
+++ Assets/ColorManager.cs
@@ -12,37 +12,45 @@ public class ColorManager : MonoBehaviour {
 
 	public Material[] materials;
 
-	// Use this for initialization
+
+
 	void Start () {
 		original_colors = new Color[colors.Length];
 		new_colors = new Color[colors.Length];
 		colors.CopyTo(original_colors, 0);
 		colors.CopyTo(new_colors, 0);
-		StartCoroutine (ChangeColors());
+        
+        StartCoroutine (ChangeColors());
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		current_hue_offset = Mathf.Lerp (current_hue_offset, hue_offset, Time.deltaTime);
 
 		if (Mathf.Abs( current_hue_offset - hue_offset) > 0.01f) {
 			for (int i = 0; i < colors.Length; i++) {
+				
+				// The color changing code
 				CIELCH cie = cs.RGBtoCIELCH (original_colors [i]);
 				cie.h += current_hue_offset;
 				colors [i] = cs.CIELCHtoRGB (cie);
+
 			}
 
-			for (int i = 0; i < colors.Length; i++)
+			for (int i = 0; i < colors.Length - 1; i++)
 				materials [i].color = colors [i];
-		}
-	}
+
+            Camera.main.backgroundColor = colors[colors.Length - 1];
+
+        }
+    }
 
 	IEnumerator ChangeColors()
 	{
-		yield return new WaitForSeconds(5);
+		
 		hue_offset += Random.Range(60, 120);
 
-		StartCoroutine (ChangeColors());
+        yield return new WaitForSeconds(3);
+        StartCoroutine (ChangeColors());
 		yield return null;
 	}
 }
